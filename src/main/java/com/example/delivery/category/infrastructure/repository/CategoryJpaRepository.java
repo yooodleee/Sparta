6c98e99fa92ead1f8b39f9d4d7523b6e7037f7d1
seola12e <dlcodus1201@naver.com>
2026-04-21 11:00:33 +0900

    fix: CategoryJpaRepository PK 타입을 UUID로 수정 (#12)

--- src/main/java/com/example/delivery/category/infrastructure/repository/CategoryJpaRepository.java
+++ src/main/java/com/example/delivery/category/infrastructure/repository/CategoryJpaRepository.java
@@ -3,5 +3,7 @@ package com.example.delivery.category.infrastructure.repository;
 import com.example.delivery.category.domain.entity.CategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryJpaRepository extends JpaRepository<CategoryEntity, Long> {
+import java.util.UUID;
+
+public interface CategoryJpaRepository extends JpaRepository<CategoryEntity, UUID> {
 }
